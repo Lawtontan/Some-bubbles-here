382e117abc4e4f00b954ed822b462d74b26907ff
Lawtontan <lawton061016@gmail.com>
2025-01-25 14:27:52 +0800

    Added ability to pick up and down, and spawn bubble
    Updated ButtonInteractionManager script, to allow debug testing of picking up and down bubble, and ability to spawn a new bubble at the given position

--- Some_Bubbles_Here/Assets/ChiFan/BubbleInteraction/BubbleInteraction_Manager.cs
+++ Some_Bubbles_Here/Assets/ChiFan/BubbleInteraction/BubbleInteraction_Manager.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class BubbleInteraction_Manager : MonoBehaviour
 {
+    public GameObject envBubble_prefab;
+    public Transform attachPoint_bubble;
+    public float pickUpAndDownBubble_duration;
+
+    private Transform currentlyOnhand;
+    private EnvBubble_Behaviour currentlyOnhand_behaviour;
+    private float currentlyOnHand_initHeight;
+
+    private void Awake()
+    {
+        BubblePool.instance = envBubble_prefab.GetComponentInChildren<EnvBubble_Behaviour>();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +33,16 @@ public class BubbleInteraction_Manager : MonoBehaviour
     /// </summary>
     /// <param name="bubble">Transform of the bubble to pick up</param>
     public void PickUpBubble(Transform bubble){
+        bubble.parent = attachPoint_bubble;
+        currentlyOnhand = bubble;
+        currentlyOnHand_initHeight = bubble.position.y;
 
+        currentlyOnhand_behaviour = bubble.GetComponentInChildren<EnvBubble_Behaviour>();
+        if(currentlyOnhand_behaviour != null)
+        {
+            currentlyOnhand_behaviour.enabled = false;
+        }
+        StartCoroutine(MoveObject(bubble, attachPoint_bubble.position));
     }
 
     /// <summary>
@@ -29,6 +50,17 @@ public class BubbleInteraction_Manager : MonoBehaviour
     /// </summary>
     public void PutDownBubble(){
 
+        if(currentlyOnhand_behaviour != null)
+        {
+            currentlyOnhand_behaviour.enabled = true;
+        }
+
+        Vector3 destination = currentlyOnhand.position;
+        destination.y = currentlyOnHand_initHeight;
+        StartCoroutine(MoveObject(currentlyOnhand, destination));
+
+        currentlyOnhand.parent = null;
+        currentlyOnhand = null;
     }
 
     /// <summary>
@@ -37,11 +69,25 @@ public class BubbleInteraction_Manager : MonoBehaviour
     /// <param name="position">Vector3 position to spawn the bubble object</param>
     public void SpawnBubble(Vector3 position){
 
+        SoundPlayer.PlayPlayerSpawnBubble();
+        BubblePool.GetBubble().transform.position = position;
+    }
+
+    IEnumerator MoveObject(Transform trans, Vector3 destination)
+    {
+        float speed = Vector3.Distance(trans.position, destination) / pickUpAndDownBubble_duration;
+        
+        while(trans.position != destination)
+        {
+            trans.position = Vector3.MoveTowards(trans.position, destination, speed * Time.deltaTime);
+            yield return null;
+        }
     }
 }
 
 public static class BubblePool
 {
+    public static EnvBubble_Behaviour instance;
     static Queue<EnvBubble_Behaviour> pool = new();
 
     public static void ResetBubble(EnvBubble_Behaviour bubble)
@@ -53,6 +99,11 @@ public static class BubblePool
 
     public static GameObject GetBubble()
     {
+        if(pool.Count == 0)
+        {
+            return GameObject.Instantiate(instance.gameObject);
+        }
+
         return pool.Dequeue().gameObject;
     }
 }
