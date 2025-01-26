351bcf03715f503f05e3ee977c0a46fdc54a438f
Lawtontan <lawton061016@gmail.com>
2025-01-26 14:45:29 +0800

    Added enemy spawning from the edge

--- Some_Bubbles_Here/Assets/ChiFan/Enemy/Enemy_Manager.cs
+++ Some_Bubbles_Here/Assets/ChiFan/Enemy/Enemy_Manager.cs
@@ -7,21 +7,39 @@ public class Enemy_Manager : MonoBehaviour
     public ControlPanel panel;
     public Enemy_Behaviour enemy_prefax;
 
+    public Transform topLeft_map;
+    public Transform bottomRight_map;
+
+    private float min_height, min_width, max_height, max_width;
+    private float minSpawnInterval, maxSpawnInterval;
+    private float spawnTriggerTime;
     private void Awake()
     {
         EnemyPool.instance = enemy_prefax;
+
+        min_height = bottomRight_map.position.z;
+        min_width = topLeft_map.position.x;
+        max_height = topLeft_map.position.z;
+        max_width = bottomRight_map.position.z;
+
+        minSpawnInterval = panel.minSpawnInterval_enemy;
+        maxSpawnInterval = panel.maxSpawnInterval_enemy;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if(Time.time >= spawnTriggerTime)
+        {
+            spawnTriggerTime = Time.time + Random.Range(minSpawnInterval, maxSpawnInterval);
+            SpawnEnemy();
+        }
     }
 
     public void RaiseTargetLost(Enemy_Behaviour enemy)
@@ -30,7 +48,7 @@ public class Enemy_Manager : MonoBehaviour
         Transform nearestBubble = null;
 
         float currentNearest = int.MaxValue;
-        foreach(var bubbleParent in BubblePool.activeEnvBubblesParent)
+        foreach (var bubbleParent in BubblePool.activeEnvBubblesParent)
         {
             float distance = Vector3.Distance(enemy_pos, bubbleParent.position);
             if (distance < currentNearest)
@@ -40,7 +58,7 @@ public class Enemy_Manager : MonoBehaviour
             }
         }
 
-        if(nearestBubble != null)
+        if (nearestBubble != null)
         {
             enemy.SetAttackTarget(nearestBubble);
         }
@@ -48,7 +66,38 @@ public class Enemy_Manager : MonoBehaviour
 
     public void SpawnEnemy()
     {
-        EnemyPool.GetEnemy().transform.position = new(Random.Range(-3, 3), 5, Random.Range(-3, 3));
+        int edgeIndex = Random.Range(0, 4);
+        Vector3 position = Vector3.zero;
+
+        switch (edgeIndex)
+        {
+            //left edge
+            case (0):
+                position.x = min_width;
+                position.z = Random.Range(min_height, max_height);
+                break;
+
+            //right edge
+            case (1):
+                position.x = max_width;
+                position.z = Random.Range(min_height, max_height);
+                break;
+
+            //top edge
+            case (2):
+                position.x = Random.Range(min_width, max_width);
+                position.z = max_height;
+                break;
+
+            //bottom edge
+            default:
+                position.x = Random.Range(min_width, max_width);
+                position.z = min_height;
+                break;
+        }
+
+        position.y = topLeft_map.position.y;
+        EnemyPool.GetEnemy().transform.position = position;
     }
 }
 
